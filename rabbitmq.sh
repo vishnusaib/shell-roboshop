5bb6a9e44c3df8ce0dae9b51f65366f15fb43aa4
vishnusaib <sri.vishnusai9@gmail.com>
2025-06-04 22:11:02 +0530

    hello world started

--- rabbitmq.sh
+++ rabbitmq.sh
@@ -21,6 +21,8 @@ else
     echo "You are running with root access" | tee -a $LOG_FILE
 fi
 
+echo "Please enter user password"
+read -s RABBIT_PASSWD
 # validate functions takes input as exit status, what command they tried to install
 VALIDATE(){
     if [ $1 -eq 0 ]
@@ -34,12 +36,12 @@ VALIDATE(){
 
 cp $SCRIPT_DIR/rabbitmq.repo /etc/yum.repos.d/rabbitmq.repo
 
-dnf install rabbitmq-server -y
+dnf install rabbitmq-server -y &>>$LOG_FILE
 VALIDATE $? "Installing Rabbit server"
 
-systemctl enable rabbitmq-server
-systemctl start rabbitmq-server
+systemctl enable rabbitmq-server &>>$LOG_FILE
+systemctl start rabbitmq-server &>>$LOG_FILE
 VALIDATE $? "Starting rabbitmq"
 
-rabbitmqctl add_user roboshop roboshop123
-rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*"
+rabbitmqctl add_user roboshop $RABBIT_PASSWD &>>$LOG_FILE
+rabbitmqctl set_permissions -p / roboshop ".*" ".*" ".*" &>>$LOG_FILE
