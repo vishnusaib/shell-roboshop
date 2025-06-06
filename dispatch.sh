9c40ec6e302e07db4921be738612caed139b173f
vishnusaib <sri.vishnusai9@gmail.com>
2025-06-06 20:03:32 +0530

    hello world started

--- dispatch.sh
+++ dispatch.sh
@@ -8,6 +8,7 @@ N="\e[0m"
 LOGS_FOLDER="/var/log/roboshop-logs"
 SCRIPT_NAME=$(echo $0 | cut -d "." -f1)
 LOG_FILE="$LOGS_FOLDER/$SCRIPT_NAME.log"
+SCRIPT_DIR=$PWD
 
 mkdir -p $LOGS_FOLDER
 echo "Script started executing at: $(date)" | tee -a $LOG_FILE
